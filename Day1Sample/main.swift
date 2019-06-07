e16fee2356da77b462d33b98bc77ed9564779287
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-06 20:20:06 -0400

    Initial Commit

---
+++ Day1Sample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day1Sample
+//
+//  Created by Neeraj Prasher on 2019-06-06.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
