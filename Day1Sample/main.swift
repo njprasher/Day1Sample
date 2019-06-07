4782b92227032618b27afaa8f7e583793aae293c
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-06 21:02:43 -0400

    Initial Commit

--- Day1Sample/main.swift
+++ Day1Sample/main.swift
@@ -10,3 +10,39 @@ import Foundation
 
 print("Hello, World!")
 
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+let 👍🏻 = "hello"
+
+print(π, 你好, 🐶🐮, 👍🏻)
+
+print(Int.max)
+
+print(Int.min)
+
+var a,b,c:Int
+
+a=10
+b=3
+
+c=a+b
+
+print(a, " + " , b ," = " , c)
+
+c=a-b
+
+print(a, " - " , b ," = " , c)
+
+c=a*b
+
+print(a, " * " , b ," = " , c)
+
+c=a/b;
+
+print(a, " / " , b ," = " , c)
+
+c=a%b
+
+print(a, " % " , b ," = " , c)
